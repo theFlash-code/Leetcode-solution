1beafa9d73a4c035980256ac64e55b6b42bd7849
theFlash-code <alan.tao.no1@gmail.com>
2022-03-02 09:23:48 +0800

    solution for problme 1

---
+++ 1.two-sum.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+
+        }
+    }
+}
+// @lc code=end
