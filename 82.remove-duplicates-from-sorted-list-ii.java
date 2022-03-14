82ecee729f2c54a6a241c9ddbe3547d0d5c8f2a3
theFlash-code <alan.tao.no1@gmail.com>
2022-03-14 13:16:40 +0800

    solution for problem 82

--- 82.remove-duplicates-from-sorted-list-ii.java
+++ 82.remove-duplicates-from-sorted-list-ii.java
@@ -8,17 +8,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
+        if (head == null)
+            return head;
+        ListNode res = head;
+        Stack<ListNode> st = new Stack<>();
+        while (head != null) {
+            if (st.isEmpty() || st.peek().val != head.val) {
+                st.push(head);
+            } else {
+                ListNode tgt = st.pop();
+                while (head != null && tgt.val == head.val) {
+                    head = head.next;
+                }
+                if (!st.isEmpty())
+                    st.peek().next = head;
+                else
+                    res = head;
+                st.push(head);
+            }
+            head = head == null ? null : head.next;
+        }
+        return res;
     }
 }
 // @lc code=end
-
