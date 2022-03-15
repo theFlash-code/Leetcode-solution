fe3144b18a848a4ae462d7617467e9915228b07d
theFlash-code <alan.tao.no1@gmail.com>
2022-03-16 00:05:25 +0800

    solution for problem 1249

---
+++ 1249.minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=1249 lang=java
+ *
+ * [1249] Minimum Remove to Make Valid Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int st = 0;
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length(); ++i) {
+            // System.out.println(i+" "+st+" "+sb.length());
+            if (sb.charAt(i) == '(') {
+                st++;
+            } else if (sb.charAt(i) == ')') {
+                if (st <= 0) {
+                    sb.deleteCharAt(i);
+                    i--;
+                } else {
+                    st--;
+                }
+            }
+            // System.out.println(st);
+        }
+        int i = sb.length() - 1;
+        while (st > 0 && i >= 0) {
+            if (sb.charAt(i) == '(') {
+                st--;
+                sb.deleteCharAt(i);
+            }
+            i--;
+        }
+        return sb.toString();
+    }
+}
+// @lc code=end
