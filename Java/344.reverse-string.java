f4c873bd54f6aaf236e1fb72bbf17bab4d67d7e1
theFlash-code <alan.tao.no1@gmail.com>
2022-04-03 00:49:26 +0800

    move files

--- Java/344.reverse-string.java
+++ Java/344.reverse-string.java
@@ -7,7 +7,12 @@
 // @lc code=start
 class Solution {
     public void reverseString(char[] s) {
-        
+        int len = s.length;
+        for(int i=0, j=len-1; i<len/2; ++i, --j){
+            char t = s[i];
+            s[i] = s[j];
+            s[j] = t;
+        }
     }
 }
 // @lc code=end
